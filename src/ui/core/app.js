e49674137a3ad9f575c6c706353586a9a57d0e3c
Kevin Isom <kevin.isom@gmail.com>
2019-07-25 07:19:34 +1200

    App Core

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Router } from '@reach/router'
+import { Hero, HeroBody, Section, Title, SubTitle } from '@brightleaf/elements'
+import './app.scss'
 
 const About = React.lazy(() => import('../features/about'))
 const Home = React.lazy(() => import('../features/home'))
@@ -9,15 +11,22 @@ export default class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>App</h1>
-        <React.Suspense fallback={<div>Loading</div>}>
-          <Router>
-            <Home path="/" />
-            <About path="/about" />
-            <Contact path="/contact" />
-          </Router>
-        </React.Suspense>
+        <Hero isPrimary isBold>
+          <HeroBody>
+            <Title>Forge</Title>
+            <SubTitle>Web App Development</SubTitle>
+          </HeroBody>
+        </Hero>
+        <Section>
+          <React.Suspense fallback={<div>Loading</div>}>
+            <Router>
+              <Home path="/" />
+              <About path="/about" />
+              <Contact path="/contact" />
+            </Router>
+          </React.Suspense>
+        </Section>
       </Fragment>
-  )
+    )
   }
 }
