b5ddb55468b7604ddaebd41029b8183a7e14c5db
Kevin Isom <kevin.isom@gmail.com>
2019-07-25 07:40:32 +1200

    css plugin

--- src/webpack/config.js
+++ src/webpack/config.js
@@ -35,14 +35,7 @@ module.exports = {
   resolve: {
     extensions: ['*', '.mjs', '.js', '.jsx'],
     modules: ['node_modules', 'src'],
-    alias: {
-      '_variables.sass': path.resolve(
-        __dirname,
-        '../',
-        'ui',
-        '_variables.sass'
-      ),
-    },
+    alias: {},
   },
   output: {
     path: path.join(__dirname, '/dist'),
