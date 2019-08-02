bce0234c4d6c750f3af34703dc2cf730a96c6ce3
Kevin Isom <kevin.isom@gmail.com>
2019-08-03 00:26:48 +1200

    navigate when complete

--- src/ui/features/hidden.js
+++ src/ui/features/hidden.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, TextArea, TextBox, Hidden } from 'react-form-elements'
 import { Button, Section, Title } from '@brightleaf/elements'
 import { usePost } from '@brightleaf/react-hooks/lib/use-post'
+import { navigate } from '@reach/router'
 const encode = data => {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
@@ -13,6 +14,9 @@ const ContactForm = () => {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   })
   console.info({ error, loading, complete })
+  if (complete) {
+    navigate('/')
+  }
   return (
     <Section>
       <Title>Contact</Title>
