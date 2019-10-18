28c5635a5bfa86841b103ef5541b526634b282fe
Kevin Isom <kevin.isom@gmail.com>
2019-10-18 13:47:58 +1300

    line break

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -6,5 +6,7 @@ export default () => (
     <Title> Make Things</Title>
     <SubTitle as="p">Create awesome</SubTitle>
     <Link to="/contact">Contact Me</Link>
+    <br />
+    <Link to="/confirm">Confirm Me</Link>
   </Section>
 )
