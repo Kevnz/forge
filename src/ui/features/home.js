863961ef37c4dc6ab03915fe06da5239b9600b9a
Kevin Isom <kevin.isom@gmail.com>
2019-10-27 09:29:54 +1300

    Make things

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -3,7 +3,7 @@ import { Section, Title, SubTitle } from '@brightleaf/elements'
 import { Link } from '@reach/router'
 export default () => (
   <Section>
-    <Title> Make Things</Title>
+    <Title>Make Things</Title>
     <SubTitle as="p">Create awesome</SubTitle>
     <Link to="/contact">Contact Me</Link>
     <br />
