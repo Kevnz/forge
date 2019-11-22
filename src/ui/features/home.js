d0991833f0e7964e8b0f5123181e6ced737bd778
Kevin Isom <kevin.isom@gmail.com>
2019-11-23 10:05:42 +1300

    return

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -10,48 +10,52 @@ import {
   MenuListItem,
 } from '@brightleaf/elements'
 import { Link } from '@reach/router'
-import modules from '../data/my-packages'
-export default () => (
-  <Section>
-    <Title>Make Things</Title>
-    <SubTitle as="p">Create awesome things</SubTitle>
-    <Container>
-      <br />
-      <Menu>
-        <MenuLabel>
-          <SubTitle as="p">Items</SubTitle>
-        </MenuLabel>
-        <MenuList className="menu-list">
-          <MenuListItem>
-            <Link to="/contact">Contact Form</Link>
-          </MenuListItem>
-          <MenuListItem>
-            <Link to="/confirm">Confirm Button Example</Link>
-          </MenuListItem>
-          <MenuListItem>
-            <Link to={`/package/${modules[0].slug}`}>
-              View stats for {modules[0].title}
-            </Link>
-          </MenuListItem>
-        </MenuList>
 
-        <MenuLabel>
-          <SubTitle as="p">Graphing</SubTitle>
-        </MenuLabel>
-        <MenuList className="menu-list">
-          <MenuListItem>
-            <Link to="/stats">Sparklines</Link>
-          </MenuListItem>
-          <MenuListItem>
-            <Link to="/tracking">Charting NPM Packages</Link>
-          </MenuListItem>
-          <MenuListItem>
-            <Link to={`/package/${modules[0].slug}`}>
-              Stats for {modules[0].title}
-            </Link>
-          </MenuListItem>
-        </MenuList>
-      </Menu>
-    </Container>
-  </Section>
-)
+import modules from '../data/my-packages'
+
+export default () => {
+  return (
+    <Section>
+      <Title>Make Things</Title>
+      <SubTitle>Create awesome things</SubTitle>
+      <Container>
+        <br />
+        <Menu>
+          <MenuLabel>
+            <SubTitle as="span">Items</SubTitle>
+          </MenuLabel>
+          <MenuList className="menu-list">
+            <MenuListItem>
+              <Link to="/contact">Contact Form</Link>
+            </MenuListItem>
+            <MenuListItem>
+              <Link to="/confirm">Confirm Button Example</Link>
+            </MenuListItem>
+            <MenuListItem>
+              <Link to={`/package/${modules[0].slug}`}>
+                View stats for {modules[0].title}
+              </Link>
+            </MenuListItem>
+          </MenuList>
+
+          <MenuLabel>
+            <SubTitle as="span">Graphing</SubTitle>
+          </MenuLabel>
+          <MenuList className="menu-list">
+            <MenuListItem>
+              <Link to="/stats">Sparklines</Link>
+            </MenuListItem>
+            <MenuListItem>
+              <Link to="/tracking">Charting NPM Packages</Link>
+            </MenuListItem>
+            <MenuListItem>
+              <Link to={`/package/${modules[0].slug}`}>
+                Stats for {modules[0].title}
+              </Link>
+            </MenuListItem>
+          </MenuList>
+        </Menu>
+      </Container>
+    </Section>
+  )
+}
