66b95f0eb5326d3c8ba7e7f336176441cb17def5
Kevin Isom <kevin.isom@gmail.com>
2019-11-23 10:06:13 +1300

    experiment with nav view

---
+++ src/ui/components/navigation-view.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import { useToggle } from '@brightleaf/elements'
+
+export const NavigationView = ({ children, className, isStatic }) => {
+  const [isActive, setIsActive] = useToggle(false)
+  return (
+    <div
+      className={classnames('navigation-view', className, {
+        'is-static': isStatic,
+        'is-active': isActive,
+      })}
+    >
+      <a
+        className={classnames('navbar-burger', { 'is-active': !isActive })}
+        role="button"
+        aria-expanded="false"
+        aria-label="menu"
+        onClick={() => {
+          setIsActive(!isActive)
+        }}
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+
+      {children}
+    </div>
+  )
+}
+
+NavigationView.propTypes = {
+  className: PropTypes.string,
+  isStatic: PropTypes.bool,
+}
+
+NavigationView.defaultProps = {
+  isStatic: true,
+}
+
+export default NavigationView
