2e7df936092c91f342574032f8b23336bbe93049
Kevin Isom <kevin.isom@gmail.com>
2019-11-29 08:44:16 +1300

    directory

--- src/scripts/reports.js
+++ src/scripts/reports.js
@@ -2,14 +2,14 @@ const reporter = require('cucumber-html-reporter')
 const cucumberJunitConvert = require('cucumber-junit-convert')
 
 const junitOpts = {
-  inputJsonFile: 'reports/cucumber_report.json',
-  outputXmlFile: 'reports/cucumber_report.xml',
+  inputJsonFile: '~/forge/reports/cucumber_report.json',
+  outputXmlFile: '~/forge/reports/cucumber_report.xml',
 }
 
 const options = {
   theme: 'bootstrap',
-  jsonFile: 'reports/cucumber_report.json',
-  output: 'reports/cucumber_report.html',
+  jsonFile: '~/forge/reports/cucumber_report.json',
+  output: '~/forge/reports/cucumber_report.html',
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: true,
