3ddce3ed3bcc5d84f284e836e85f18aeb09d8866
Kevin Isom <kevin.isom@gmail.com>
2019-11-29 17:28:27 +1300

    attach to world

--- src/tests/support/world.js
+++ src/tests/support/world.js
@@ -1,10 +1,16 @@
 const { setWorldConstructor } = require('cucumber')
+
 const puppeteer = require('puppeteer')
+const { toMatchImageSnapshot } = require('jest-image-snapshot')
 
 const expect = require('expect-puppeteer')
+// expect.extend({ toMatchImageSnapshot })
+
 const scope = require('./scope')
 
-const World = function() {
+const World = function({ attach, parameters }) {
+  this.attach = attach
+  this.parameters = parameters
   scope.driver = puppeteer
   scope.host = 'http://localhost:8080'
   scope.context = {}
