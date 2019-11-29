76a235801fae15e512b8103c91e18d53b1568518
Kevin Isom <kevin.isom@gmail.com>
2019-11-29 16:18:51 +1300

    Test the stats

--- src/tests/step_definitions/stats.js
+++ src/tests/step_definitions/stats.js
@@ -1,17 +1,22 @@
 const { When, Then } = require('cucumber')
 const { delay } = require('@kev_nz/async-tools')
-const { hasText } = require('../support/actions')
+const { hasText, matchScreenshot } = require('../support/actions')
 
-Then('the page should display the correct totals', async function() {
-  // Write code here that turns the phrase above into concrete actions
-  await delay(800)
-  return Promise.all([
-    hasText('Brightleaf Elements Total: 17058'),
-    hasText('Brightleaf Hooks Total: 19023'),
-    hasText('React Form Elements Total: 20640'),
-    hasText('Async Tools Total: 17719'),
-    hasText('760 - 681 - 766 - 676 - 702 - 737 - 679'),
-    hasText('Back-Off Total: 12869'),
-    hasText('Creature Features Total: 13276'),
-  ])
-})
+Then(
+  'the page should display the correct totals',
+  { timeout: 60 * 1000 },
+  async function() {
+    // Write code here that turns the phrase above into concrete actions
+    await delay(800)
+    return Promise.all([
+      hasText('Brightleaf Elements Total: 17058'),
+      hasText('Brightleaf Hooks Total: 19023'),
+      hasText('React Form Elements Total: 20640'),
+      hasText('Async Tools Total: 17719'),
+      hasText('760 - 681 - 766 - 676 - 702 - 737 - 679'),
+      hasText('Back-Off Total: 12869'),
+      hasText('Creature Features Total: 13276'),
+      matchScreenshot('stats'),
+    ])
+  }
+)
