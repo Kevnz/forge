a19cd140c50256d26bb8d7835890fca66bf7f9b2
Kevin <kevin.isom@gmail.com>
2020-07-09 13:13:35 +1200

    Update merge require

--- src/webpack/dev.config.js
+++ src/webpack/dev.config.js
@@ -1,7 +1,7 @@
 require('xtconf')()
 const path = require('path')
 const webpack = require('webpack')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
