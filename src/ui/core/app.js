329378d78c0189def7396304e1c1ae769fa32acc
Kevin <kevin.isom@gmail.com>
2020-08-13 11:38:54 +1200

    Use function instead

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -50,141 +50,139 @@ const UpLink = props => {
     </Location>
   )
 }
-export default class App extends Component {
-  render() {
-    const url = 'https://forge.kevnz.xyz'
-    return (
-      <IdentityContextProvider url={url}>
-        <LocationProvider history={history}>
-          <Fragment>
-            <NavigationView isStatic>
-              <Menu>
-                <MenuLabel>General</MenuLabel>
-                <MenuList className="menu-list">
+export default function App() {
+  const url = 'https://forge.kevnz.xyz'
+  return (
+    <IdentityContextProvider url={url}>
+      <LocationProvider history={history}>
+        <Fragment>
+          <NavigationView isStatic>
+            <Menu>
+              <MenuLabel>General</MenuLabel>
+              <MenuList className="menu-list">
+                <MenuListItem>
+                  <UpLink to="/">
+                    <Icon fas icon="home" /> Home
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/contact">
+                    <Icon fas icon="inbox" /> Contact
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/confirm">
+                    <Icon fas icon="check-circle" /> Confirm
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/backoff">
+                    <Icon fas icon="redo" /> Back Off
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/stats">
+                    <Icon fas icon="chart-line" /> NPM Overview
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/tracking">
+                    <Icon fas icon="chart-pie" /> NPM Details
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to={`/package/${modules[0].slug}`}>
+                    <Icon fas icon="cogs" /> Module Stats
+                  </UpLink>
+                </MenuListItem>
+                <MenuListItem>
+                  <UpLink to="/animations">
+                    <Icon fas icon="bacon" /> Animation
+                  </UpLink>
+                </MenuListItem>
+                {FEATURES && FEATURES.FeatureFlags && (
                   <MenuListItem>
-                    <UpLink to="/">
-                      <Icon fas icon="home" /> Home
+                    <UpLink to="/feature-flags">
+                      <Icon fas icon="gem" /> Feature Flags
                     </UpLink>
                   </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/contact">
-                      <Icon fas icon="inbox" /> Contact
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/confirm">
-                      <Icon fas icon="check-circle" /> Confirm
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/backoff">
-                      <Icon fas icon="redo" /> Back Off
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/stats">
-                      <Icon fas icon="chart-line" /> NPM Overview
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/tracking">
-                      <Icon fas icon="chart-pie" /> NPM Details
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to={`/package/${modules[0].slug}`}>
-                      <Icon fas icon="cogs" /> Module Stats
-                    </UpLink>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <UpLink to="/animations">
-                      <Icon fas icon="bacon" /> Animation
-                    </UpLink>
-                  </MenuListItem>
-                  {FEATURES && FEATURES.FeatureFlags && (
-                    <MenuListItem>
-                      <UpLink to="/feature-flags">
-                        <Icon fas icon="gem" /> Feature Flags
-                      </UpLink>
-                    </MenuListItem>
-                  )}
-                  <MenuListItem>
-                    <LoginLink />
-                  </MenuListItem>
-                  <AdminMenuItem />
-                </MenuList>
-                <br />
-                <MenuLabel>
-                  <Icon fas icon="book" /> Documentation
-                </MenuLabel>
-                <MenuList className="menu-list">
-                  <MenuListItem>
-                    <a href="https://brightleaf.dev/elements">
-                      <Icon fas icon="lightbulb" /> Brightleaf Elements
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://brightleaf.dev/hooks">
-                      <Icon fas icon="sun" /> React Hooks
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://kevinisom.info/react-form-elements">
-                      <Icon fas icon="cubes" /> Form Elements
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://kevinisom.info/creature-features">
-                      <Icon fas icon="pastafarianism" /> Creature Features
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://kevinisom.info/back-off">
-                      <Icon fas icon="redo" /> Back-Off
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://kevinisom.info/async-tools">
-                      <Icon fas icon="tools" /> Async Tools
-                    </a>
-                  </MenuListItem>
-                  <MenuListItem>
-                    <a href="https://github.com/kevnz/">
-                      <Icon fab icon="github" /> GitHub
-                    </a>
-                  </MenuListItem>
-                </MenuList>
-              </Menu>
-            </NavigationView>
+                )}
+                <MenuListItem>
+                  <LoginLink />
+                </MenuListItem>
+                <AdminMenuItem />
+              </MenuList>
+              <br />
+              <MenuLabel>
+                <Icon fas icon="book" /> Documentation
+              </MenuLabel>
+              <MenuList className="menu-list">
+                <MenuListItem>
+                  <a href="https://brightleaf.dev/elements">
+                    <Icon fas icon="lightbulb" /> Brightleaf Elements
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://brightleaf.dev/hooks">
+                    <Icon fas icon="sun" /> React Hooks
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://kevinisom.info/react-form-elements">
+                    <Icon fas icon="cubes" /> Form Elements
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://kevinisom.info/creature-features">
+                    <Icon fas icon="pastafarianism" /> Creature Features
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://kevinisom.info/back-off">
+                    <Icon fas icon="redo" /> Back-Off
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://kevinisom.info/async-tools">
+                    <Icon fas icon="tools" /> Async Tools
+                  </a>
+                </MenuListItem>
+                <MenuListItem>
+                  <a href="https://github.com/kevnz/">
+                    <Icon fab icon="github" /> GitHub
+                  </a>
+                </MenuListItem>
+              </MenuList>
+            </Menu>
+          </NavigationView>
 
-            <NavBar isPrimary isFixedTop>
-              <NavBarBrand
-                className="flame"
-                src="/flames-small.png"
-                href="/"
-                target="navbarBasicExample"
-                title="The Forge"
-                width="32"
-                height="32"
-              />
-            </NavBar>
+          <NavBar isPrimary isFixedTop>
+            <NavBarBrand
+              className="flame"
+              src="/flames-small.png"
+              href="/"
+              target="navbarBasicExample"
+              title="The Forge"
+              width="32"
+              height="32"
+            />
+          </NavBar>
 
-            <Section>
-              <React.Suspense
-                fallback={
-                  <div>
-                    <br />
-                    <br />
-                    <Loader isSize1 />
-                  </div>
-                }
-              >
-                <Routes />
-              </React.Suspense>
-            </Section>
-          </Fragment>
-        </LocationProvider>
-      </IdentityContextProvider>
-    )
-  }
+          <Section>
+            <React.Suspense
+              fallback={
+                <div>
+                  <br />
+                  <br />
+                  <Loader isSize1 />
+                </div>
+              }
+            >
+              <Routes />
+            </React.Suspense>
+          </Section>
+        </Fragment>
+      </LocationProvider>
+    </IdentityContextProvider>
+  )
 }
